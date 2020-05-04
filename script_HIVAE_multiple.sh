557c53bd17fe6811ecd7db8f761eff44c2a52360
Elias Panula <elias.panula@gmail.com>
2020-05-04 18:54:18 +0300

    master Modify start up script

--- script_HIVAE_multiple.sh
+++ script_HIVAE_multiple.sh
@@ -1,4 +1,5 @@
-#source ~/venv/bin/activate
+source ./env/Scripts/activate
+
 declare dataset="ICU"
 declare m_perc=20
 declare mask=1
@@ -18,14 +19,14 @@ train_model(){
     python main_scripts.py --model_name $1 --batch_size $5 --epochs ${10} \
     --data_file $6 --types_file $7 --miss_file $8 \
     --dim_latent_z $2 --dim_latent_y $3 --dim_latent_s $4 \
-    --save_file $9 \
+    --save_file $9
 }
 
 test_model(){
     python main_scripts.py --model_name $1 --batch_size 10000000 --epochs 1 \
     --data_file $5 --types_file $6 --miss_file $7 \
     --dim_latent_z $2 --dim_latent_y $3 --dim_latent_s $4 \
-    --save_file $8 --train 0 --restore 1 \
+    --save_file $8 --train 0 --restore 1
 }
 
 # train and test first model
@@ -33,8 +34,8 @@ declare z_dim=2
 declare y_dim=5
 declare s_dim=10
 declare batch_size=128
-declare epochs=1
-declare save_file=${model}_${dataset}_Missing${m_perc}_${mask}_z${z_dim}_y${y_dim}_s${s_dim}_batch${batch_size}_epocs${epochs}
+declare epochs=200
+declare save_file=${model}_${dataset}_Missing${m_perc}_${mask}_z${z_dim}_y${y_dim}_s${s_dim}_batch${batch_size}_epochs${epochs}
 
 train_model ${model} ${z_dim} ${y_dim} ${s_dim} ${batch_size} ${data_file} ${types_file} ${miss_file} ${save_file} ${epochs}
 test_model ${model} ${z_dim} ${y_dim} ${s_dim} ${test_data_file} ${types_file} ${test_miss_file} ${save_file}
@@ -43,9 +44,9 @@ test_model ${model} ${z_dim} ${y_dim} ${s_dim} ${test_data_file} ${types_file} $
 z_dim=2
 y_dim=5
 s_dim=10
-epochs=2
+epochs=400
 batch_size=128
-save_file=${model}_${dataset}_Missing${m_perc}_${mask}_z${z_dim}_y${y_dim}_s${s_dim}_batch${batch_size}_epocs${epochs}
+save_file=${model}_${dataset}_Missing${m_perc}_${mask}_z${z_dim}_y${y_dim}_s${s_dim}_batch${batch_size}_epochs${epochs}
 
 train_model ${model} ${z_dim} ${y_dim} ${s_dim} ${batch_size} ${data_file} ${types_file} ${miss_file} ${save_file} ${epochs}
 test_model ${model} ${z_dim} ${y_dim} ${s_dim} ${test_data_file} ${types_file} ${test_miss_file} ${save_file}
@@ -54,9 +55,9 @@ test_model ${model} ${z_dim} ${y_dim} ${s_dim} ${test_data_file} ${types_file} $
 z_dim=2
 y_dim=5
 s_dim=10
-epochs=3
+epochs=800
 batch_size=128
-save_file=${model}_${dataset}_Missing${m_perc}_${mask}_z${z_dim}_y${y_dim}_s${s_dim}_batch${batch_size}_epocs${epochs}
+save_file=${model}_${dataset}_Missing${m_perc}_${mask}_z${z_dim}_y${y_dim}_s${s_dim}_batch${batch_size}_epochs${epochs}
 
 train_model ${model} ${z_dim} ${y_dim} ${s_dim} ${batch_size} ${data_file} ${types_file} ${miss_file} ${save_file} ${epochs}
 test_model ${model} ${z_dim} ${y_dim} ${s_dim} ${test_data_file} ${types_file} ${test_miss_file} ${save_file}
